e60cfc9a08ae95531b2e9e42188d9db69f241a32
Jeet Rami <jeetrami39@gmail.com>
2018-11-20 19:36:03 +0530

    activate all corpus

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ conversation = [
 chatbot.set_trainer(ListTrainer)
 chatbot.train(conversation)
 chatbot.set_trainer(ChatterBotCorpusTrainer)
-"""
+
 chatbot.train(
     "chatterbot.corpus.english.ai",
     "chatterbot.corpus.english.botprofile",
@@ -57,7 +57,7 @@ chatbot.train(
     "chatterbot.corpus.english.psychology",
     "chatterbot.corpus.english.greetings",
 )
-"""
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
 	if (request.method == 'POST'):
